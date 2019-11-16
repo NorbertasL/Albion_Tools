08af3e1ce589168399024f3fc39eb9200efcf27c
NorbertasL <nlapenas@gmail.com>
2019-11-16 11:34:29 +0000

    Cleaned up code.

--- src/main/java/com/redspark/albiontools/pricer/ImageScanner.java
+++ src/main/java/com/redspark/albiontools/pricer/ImageScanner.java
@@ -9,55 +9,52 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 
 public class ImageScanner {
 
+    @SuppressWarnings("FieldCanBeLocal")
+    private final String tessarachDataLocation= "X:\\Programming\\External Lib";
+
+    @SuppressWarnings("unused")
     private Rectangle nameArea, slotArea, qualityArea, slotArea2, itemPowerArea, weightArea;
     private Robot robot;
     private Tesseract tesseract;
 
     private BufferedImage capture;
-    public ImageScanner(){
+    ImageScanner(){
         try {
             robot = new Robot();
         }catch (AWTException e){
-            System.out.println(e);
+            e.printStackTrace();
         }
         tesseract = new Tesseract();
-        tesseract.setDatapath("X:\\Programming\\External Lib");
+        tesseract.setDatapath(tessarachDataLocation);
         nameArea = new Rectangle(845,300,330,22);
         slotArea = new Rectangle(nameArea.x, nameArea.y+53, nameArea.width/2, nameArea.height);
 
-
-
     }
 
-    public static void Capture() throws AWTException, IOException, TesseractException {
-        Rectangle screenRect = new Rectangle(845,300,330,30);
-        BufferedImage capture = new Robot().createScreenCapture(screenRect);
-        ImageIO.write(capture, "bmp", new File("X:\\Programming\\External Lib\\Test.bmp"));
-        Tesseract tesseract = new Tesseract();
-        tesseract.setDatapath("X:\\Programming\\External Lib");
-        System.out.println(tesseract.doOCR(capture));
-
-    }
-    public String getTitle(){
+    String getTitle(){
         capture = robot.createScreenCapture(nameArea);
         writeImage("Name");
         String name = getText(capture);
-        name = name.isEmpty() ? "None" : trimString(name);
+        name = name == null || name.isEmpty() ? "None" : trimString(name);
         System.out.println("Item Name:"+name);
         return name;
     }
+    @SuppressWarnings("unused")
     public String getSlot(){
         capture = robot.createScreenCapture(slotArea);
         String slot = getText(capture);
+
+        //TODO scan second location
 //        if(slot.isEmpty()){
 //            capture = robot.createScreenCapture(slotArea2);
 //        }
         writeImage("Slot");
-        slot = slot.isEmpty() || slot.split(":").length < 2 ? "None" : trimString(slot.split(":")[1]);
+        slot = slot == null
+                || slot.isEmpty()
+                || slot.split(":").length < 2 ? "None" : trimString(slot.split(":")[1]);
         System.out.println("Item Slot:"+slot);
         return slot;
     }
@@ -65,18 +62,18 @@ public class ImageScanner {
         try {
             return tesseract.doOCR(img);
         }catch (TesseractException e){
-            System.out.println(e);
+            e.printStackTrace();
         }
         return null;
     }
     private void writeImage(String fileName){
         try {
-            ImageIO.write(capture, "bmp", new File("X:\\Programming\\Albion Tools\\DEBUG STUFF\\"+fileName+".bmp"));
+            ImageIO.write(capture, "bmp", new File(fileName+".bmp"));
         }catch (Exception e){
-            System.out.println(e);
+            e.printStackTrace();
         }
     }
     private String trimString(String s){
-        return s.replaceAll("[\\\r\\\n]+","").trim();
+        return s.replaceAll("[\r\n]+","").trim();
     }
 }
