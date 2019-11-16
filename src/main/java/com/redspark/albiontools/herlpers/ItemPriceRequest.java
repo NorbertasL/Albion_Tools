8e62258a654216ba979d146ec65362fa2b3d6d04
NorbertasL <nlapenas@gmail.com>
2019-11-16 00:54:40 +0000

    Cleaned up ItemPriceRequest.class

--- src/main/java/com/redspark/albiontools/herlpers/ItemPriceRequest.java
+++ src/main/java/com/redspark/albiontools/herlpers/ItemPriceRequest.java
@@ -5,6 +5,11 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+/**
+ * Class that does a asynchronous price request for an item
+ * After it receives the data it does a callback to the caller class with the json response
+ */
+
 public class ItemPriceRequest {
 
 
@@ -13,67 +18,67 @@ public class ItemPriceRequest {
     //Default search values are pre-set.
     private LOCATION location = LOCATION.Caerleon;
     private Item.QUALITY quality = Item.QUALITY.UNKNOWN;
-    private int enchant= 0;
+    private int enchant = 0;
 
 
-    public ItemPriceRequest(ItemRequestCallback callerClass){
-        if(!(callerClass instanceof ItemRequestCallback)){
-            DebugLog.printError(this, "Class "+ callerClass.toString()
-                    +" does not implement ItemRequestCallback");
-        }else{
+    public ItemPriceRequest(ItemRequestCallback callerClass) {
+        if (!(callerClass instanceof ItemRequestCallback)) {
+            DebugLog.printError(this, "Class " + callerClass.toString()
+                    + " does not implement ItemRequestCallback");
+        } else {
             this.callerClass = callerClass;
         }
-
-
     }
 
-    private final String baseURL ="https://www.albion-online-data.com/api/v2/stats/";
-    private final String requestType = "prices/";
-    private final String enchantTag = "@";
-    private final String locationTag = "?locations=";
-    private final String qualityTag = "&qualities=";
-    public void sendRequest(String itemID, int enchant){
-        this.enchant =enchant;
+    public void sendRequest(String itemID, int enchant) {
+        this.enchant = enchant;
         HttpClient client = HttpClient.newHttpClient();
-        //String url = "https://www.albion-online-data.com/api/v2/stats/prices/"+itemID+"?locations="+location.name()+"&qualities=";
-        //System.out.println("Sending request with this URL:");
-        //System.out.println(url);
+
+        //Asynchronous data request
         HttpRequest request = HttpRequest.newBuilder().uri(buildUri(itemID)).build();
         client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                 .thenApply(HttpResponse::body)
                 .thenApply(callerClass::jsonResponse)
                 .join();
     }
-    private URI buildUri(String itemID){
+
+    private final String baseURL = "https://www.albion-online-data.com/api/v2/stats/";
+    private final String requestType = "prices/";
+    private final String enchantTag = "@";
+    private final String locationTag = "?locations=";
+    private final String qualityTag = "&qualities=";
+
+    //Builds URI based on the pre-defined settings
+    private URI buildUri(String itemID) {
         String uri = baseURL;
-        uri+= requestType;
+        uri += requestType;
 
-        uri+=itemID;
+        uri += itemID;
 
-        if(enchant != 0){
-            uri+=enchantTag+enchant;
+        if (enchant != 0) {
+            uri += enchantTag + enchant;
         }
-        uri+=locationTag+location.getLocationString();
-        uri+=qualityTag;
-        if(quality != Item.QUALITY.UNKNOWN){
-            uri+=quality.getQualityIndex();
+        uri += locationTag + location.getLocationString();
+        uri += qualityTag;
+        if (quality != Item.QUALITY.UNKNOWN) {
+            uri += quality.getQualityIndex();
         }
         URI returnURI = URI.create(uri);
-        System.out.println("URI:"+returnURI);
+        System.out.println("URI:" + returnURI);
         return returnURI;
     }
 
-    public void setLocation(LOCATION location){
+    public void setLocation(LOCATION location) {
         this.location = location;
     }
-    public LOCATION getLocation(){
+
+    public LOCATION getLocation() {
         return location;
     }
 
 
-
-
-    public enum LOCATION{
+    //Enum of supported locations
+    public enum LOCATION {
         Caerleon("Caerleon"),
         Thetford("Thetford"),
         Lymhurst("Lymhurst"),
@@ -81,14 +86,15 @@ public class ItemPriceRequest {
         Martlock("Martlock");
 
         private String string;
-        LOCATION(String string){
+
+        LOCATION(String string) {
             this.string = string;
         }
-        public String getLocationString(){
+
+        public String getLocationString() {
             return string;
         }
     }
-
 
 
 }
